c96575f1134160e92b0699a361b26bea567f15c2
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-10-02 15:00:25 -0400

    Header

--- src/DeltaLake/Table/EngineOptions.cs
+++ src/DeltaLake/Table/EngineOptions.cs
@@ -1,3 +1,14 @@
+// -----------------------------------------------------------------------------
+// <summary>
+// Centralized configuration for Delta Engine.
+// </summary>
+//
+// <copyright company="The Delta Lake Project Authors">
+// Copyright (2024) The Delta Lake Project Authors.  All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------------
+
 namespace DeltaLake.Table
 {
     /// <summary>
