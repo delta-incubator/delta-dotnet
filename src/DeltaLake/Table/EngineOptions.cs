95c07ada6bf328cd84a7bcce53961fee5b208211
Derpius <49565664+Derpius@users.noreply.github.com>
2026-03-21 14:44:14 +0000

    Resolve linter warnings due to explicitly initialised defaults

--- src/DeltaLake/Table/EngineOptions.cs
+++ src/DeltaLake/Table/EngineOptions.cs
@@ -24,22 +24,22 @@ namespace DeltaLake.Table
         /// <summary>
         /// If non-zero, sets the batch size of DataFusion to this value.
         /// </summary>
-        public ulong DataFusionExecutionBatchSize { get; init; } = 0;
+        public ulong DataFusionExecutionBatchSize { get; init; }
 
         /// <summary>
         /// If non-zero, sets the maximum memory (in bytes) available to DataFusion before disk spilling occurs.
         /// </summary>
-        public ulong DataFusionRuntimeMaxSpillSize { get; init; } = 0;
+        public ulong DataFusionRuntimeMaxSpillSize { get; init; }
 
         /// <summary>
         /// If defined, sets the temp directory DataFusion will use for disk spilling.
         /// </summary>
-        public string? DataFusionRuntimeTempDirectory { get; init; } = null;
+        public string? DataFusionRuntimeTempDirectory { get; init; }
 
         /// <summary>
         /// If non-zero, sets the maximum disk space (in bytes) that the DataFusion temp directory can use.
         /// </summary>
         /// <seealso cref="DataFusionRuntimeTempDirectory"/>
-        public ulong DataFusionRuntimeMaxTempDirectorySize { get; init; } = 0;
+        public ulong DataFusionRuntimeMaxTempDirectorySize { get; init; }
     }
 }
